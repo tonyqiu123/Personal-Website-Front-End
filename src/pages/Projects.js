3620a902fe0ed95356ab89bee4a0ec61969df8c4
tonyqiu123 <tonyqiu12345@gmail.com>
2023-01-23 00:03:22 -0500

    removed project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,7 +19,7 @@ function Projects() {
           </div>
     {/* media quries */}
     {/* tailwind css */}
-          <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1063733144957763638/image_17.png" name="UX Heat Map Website" tools="React, Node, Express, MongoDB, Redis" responsiblities="UX Visualizer Program" date="Jan 2023" href="" />
+          <Project image="" name="UX Heat Map Website" tools="React, Node, Express, MongoDB, Redis" responsiblities="UX Visualizer Program" date="Jan 2023" href="" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963815907399/image_26.png" name="Sip" tools="HTML, CSS, JavaScript" responsiblities="Downloadable Software Website" date="Aug 2022" href="https://sipapp.io/" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963534905414/image_24.png" name="Friend Accounting" tools="HTML, CSS, JavaScript" responsiblities="Tax Accounting Firm Website" date="Jul 2022" href="https://friendaccounting.com/en/index" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087964084359188/image_27.png" name="Tony's Web Designs" tools="HTML, CSS, JavaScript" responsiblities="Personal Business Website" date="Jun 2022" href="https://tonyswebdesigns.ca/" />
