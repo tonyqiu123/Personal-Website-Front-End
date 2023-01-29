99b21bb453fb1c828259a181e2efcf7debf9f569
tonyqiu123 <tonyqiu12345@gmail.com>
2023-01-28 21:25:34 -0500

    added skeleton loading animation to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import '../css/home.css';
 import '../css/all.css'
 import AnimatedPage from '../components/AnimatedPage'
-import HomeProject from '../components/HomeProject';
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
 
   return (
     <AnimatedPage>
-
+        {loading && 
+          <div className='homeContainer'>
+            <div className='loadingSkeleton'></div>
+            <div className='loadingSkeleton'></div>
+            <div className='loadingSkeleton'></div>
+            <div className='loadingSkeleton'></div>
+          </div>
+          }
         <div className='homeContainer'>
-          <HomeProject href="https://brightbearsinfo.com/" image="https://cdn.discordapp.com/attachments/715319623637270638/1064087964411498616/image_34.png" />
-          <HomeProject href="https://tonyswebdesigns.ca/" image="https://cdn.discordapp.com/attachments/715319623637270638/1064087964084359188/image_27.png" />
-          <HomeProject href="https://sipapp.io/" image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963815907399/image_26.png" />
-          <HomeProject href="https://friendaccounting.com/en/index" image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963534905414/image_24.png" />
+          <a onLoad={() => setLoading(false)} href="https://brightbearsinfo.com/"  target="_blank" className='HomeProjectContainer'>
+              <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087964411498616/image_34.png" />
+          </a>
+          <a onLoad={() => setLoading(false)} href="https://tonyswebdesigns.ca/"   target="_blank" className='HomeProjectContainer'>
+              <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087964084359188/image_27.png" />
+          </a>
+          <a onLoad={() => setLoading(false)} href="https://sipapp.io/"  target="_blank" className='HomeProjectContainer'>
+              <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087963815907399/image_26.png" />
+          </a>
+          <a onLoad={() => setLoading(false)} href="https://friendaccounting.com/en/index"  target="_blank" className='HomeProjectContainer'>
+              <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087963534905414/image_24.png" />
+          </a>
+ 
         </div>
     </AnimatedPage>
-
-  )
-};
+  );
+}
