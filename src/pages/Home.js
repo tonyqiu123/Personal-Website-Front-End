ec950909925ed528e8eb58f1b1d943d25428be63
tonyqiu123 <tonyqiu12345@gmail.com>
2023-02-24 14:09:22 -0500

    updated projects and home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,9 +23,6 @@ export default function Home() {
           <a onLoad={() => setLoading(false)} href="https://tonyswebdesigns.ca/"   target="_blank" className='HomeProjectContainer'>
               <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087964084359188/image_27.png" />
           </a>
-          <a onLoad={() => setLoading(false)} href="https://sipapp.io/"  target="_blank" className='HomeProjectContainer'>
-              <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087963815907399/image_26.png" />
-          </a>
           <a onLoad={() => setLoading(false)} href="https://friendaccounting.com/en/index"  target="_blank" className='HomeProjectContainer'>
               <img src="https://cdn.discordapp.com/attachments/715319623637270638/1064087963534905414/image_24.png" />
           </a>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,7 +19,6 @@ function Projects() {
           </div>
     {/* media quries */}
     {/* tailwind css */}
-          <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963815907399/image_26.png" name="Sip" tools="HTML, CSS, JavaScript" responsiblities="Downloadable Software Website" date="Aug 2022" href="https://sipapp.io/" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087963534905414/image_24.png" name="Friend Accounting" tools="HTML, CSS, JavaScript" responsiblities="Tax Accounting Firm Website" date="Jul 2022" href="https://friendaccounting.com/en/index" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087964084359188/image_27.png" name="Tony's Web Designs" tools="HTML, CSS, JavaScript" responsiblities="Personal Business Website" date="Jun 2022" href="https://tonyswebdesigns.ca/" />
           <Project image="https://cdn.discordapp.com/attachments/715319623637270638/1064087964411498616/image_34.png" name="BrightBearsInfo" tools="HTML, CSS, JavaScript" responsiblities="Educational Organization Website" date="May 2021" href="https://brightbearsinfo.com/" />
